448f8c62b0d6adf5c48022c16935d11cfb64037d
Mandela Steele-Dadzie <msteeledadzie@gmail.com>
2022-08-03 18:25:41 -0600

    pushing up code to swap out icons and add Salmon Cookies to project list

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import videobg from "./burning-flames.mp4"
 
 class Footer extends Component {
   render() {
@@ -16,57 +17,48 @@ class Footer extends Component {
 
     return (
       <footer>
-        <div className="container">
-          <div className="row">
-            <div className="col social-links">{networks}</div>
-            <div className="col">
-              <div 
-                onClick={() =>
-                  this.props.applyPickedLanguage(
-                    window.$primaryLanguage,
-                    window.$secondaryLanguageIconId
-                  )
-                }
-                style={{ display: "inline" }}
-              >
-                <span
-                  className="iconify language-icon mr-5"
-                  data-icon="twemoji-flag-for-flag-united-kingdom"
-                  data-inline="false"
-                  id={window.$primaryLanguageIconId}
-                ></span>
+        <div className="videobg">
+          <video src={videobg} autoplay loop muted />
+          <div className="container">
+            <div className="row">
+              <div className="col social-links">{networks}</div>
+              <div className="col">
+                <div 
+                  onClick={() =>
+                    this.props.applyPickedLanguage(
+                      window.$primaryLanguage,
+                      window.$secondaryLanguageIconId
+                    )
+                  }
+                  style={{ display: "inline" }}
+                >
+                </div>
+                <div 
+                  onClick={() =>
+                    this.props.applyPickedLanguage(
+                      window.$secondaryLanguage,
+                      window.$primaryLanguageIconId
+                    )
+                  }
+                  style={{ display: "inline" }}
+                >
+                </div>
               </div>
-              <div 
-                onClick={() =>
-                  this.props.applyPickedLanguage(
-                    window.$secondaryLanguage,
-                    window.$primaryLanguageIconId
-                  )
-                }
-                style={{ display: "inline" }}
-              >
-                <span
-                  className="iconify language-icon"
-                  data-icon="twemoji-flag-for-flag-poland"
-                  data-inline="false"
-                  id={window.$secondaryLanguageIconId}
-                ></span>
-              </div>
-            </div>
-            <div className="col">
-              <div className="copyright py-4 text-center">
-                <div className="container">
-                  <small>
-                    Copyright &copy;{" "}
-                    {this.props.sharedBasicInfo
-                      ? this.props.sharedBasicInfo.name
-                      : "???"}
-                  </small>
+              <div className="col">
+                <div className="copyright py-4 text-center">
+                  <div className="container">
+                    <small>
+                      Copyright &copy;{" "}
+                      {this.props.sharedBasicInfo
+                        ? this.props.sharedBasicInfo.name
+                        : "???"}
+                    </small>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+          </div>
       </footer>
     );
   }
